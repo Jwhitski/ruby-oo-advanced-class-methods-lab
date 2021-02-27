ea55a45ad4f9bf3579a3da556c24303c95e84f34
Jason Whitlock <jrwhitlock7@gmail.com>
2021-02-27 13:10:55 -0700

    commit tree 30c0c7b408d551156125e6a303583e52acc05820

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +9,56 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    self.all << self.new
+    self.all[-1]
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(song_name)
+    song = self.all.find{|i| i.name == song_name}
+    # binding.pry
+    song
+  end
+
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+  
+  def self.alphabetical
+    self.all.sort_by {|a| a.name}
+  end
+
+  def self.new_from_filename(name)
+    song = self.new
+    song.name = name.split(" - ")[1].chomp(".mp3")
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
+
+  def self.create_from_filename(name)
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    self.all << song
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
